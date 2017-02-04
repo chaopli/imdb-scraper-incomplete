8ed6361a915a21727cfc33a63a955415a66e7d41
Chao Li <cli@avinetworks.com>
2017-02-03 21:08:59 -0800

    initial commit

---
+++ imdbscraper/pipelines.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+from elasticsearch_dsl.document import DocType
+from elasticsearch_dsl.field import Text, Date
+from elasticsearch_dsl import Index
+from elasticsearch import Elasticsearch
+
+es = Elasticsearch()
+
+
+class ImdbscraperPipeline(object):
+    def __init__(self):
+        movies = Index('imdb', using=es)
+        movies.doc_type(Movie)
+        movies.delete(ignore=404)
+        movies.create()
+
+    def process_item(self, item, spider):
+        movie = Movie()
+        movie.title = item['title']
+        movie.summary = item['summary']
+        movie.datePublished = item['datePublished']
+        movie.save(using=es)
+        return item
+
+
+class Movie(DocType):
+    title = Text(fields={'raw':{'type': 'keyword'}})
+    summary = Text()
+    datePublished = Date()
+    class Meta:
+        index = 'imdb'
---
+++ imdbscraper/spiders/imdb_spider.py
@@ -0,0 +1,27 @@
+import scrapy
+import time
+import random
+
+class ImdbSpider(scrapy.Spider):
+    name = 'imdb_spider'
+    start_urls = [
+    'http://www.imdb.com/search/title?release_date=1980-01-01,2018-01-01&title_type=feature&user_rating=5.0,10'
+    ]
+    imdbhome = 'http://www.imdb.com'
+
+    def parse(self, response):
+        movie_hrefs = [self.imdbhome+x for x in response.selector.xpath('//h3[@class="lister-item-header"]/a/@href').extract()]
+        next_page = response.selector.xpath('//a[@ref-marker="adv_nxt"]/@href').extract()
+        for movie in movie_hrefs:
+            yield scrapy.Request(movie, callback=self.parse_movie)
+            time.sleep(random.randint(2,4))
+        if next_page:
+            yield scrapy.Request(self.imdbhome+next_page[0], callback=self.parse)
+
+    def parse_movie(self, response):
+        title = response.selector.xpath('//h1[@itemprop="name"]/text()').extract_first()
+        datePublished = response.selector.xpath('//meta[@itemprop="datePublished"]/@content').extract_first()
+        summary = response.selector.xpath('//div[@class="summary_text"]/text()').extract_first()
+        yield {'title': title,
+               'datePublished': datePublished,
+               'summary': summary}
